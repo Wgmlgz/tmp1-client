3350f0ca1b067bc363a374196eaa79b4a0236c83
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2022-01-17 11:29:01 +0300

    Update Dashboard.tsx

--- src/app_components/Dashboard.tsx
+++ src/app_components/Dashboard.tsx
@@ -39,54 +39,57 @@ export default function Dashboard() {
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-        <div className='logo' />
-        <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
-          <Menu.Item key='1' icon={<PieChartOutlined />}>
-            Option 1
-          </Menu.Item>
-          <Menu.Item key='2' icon={<DesktopOutlined />}>
-            Option 2
-          </Menu.Item>
-          <SubMenu key='sub1' icon={<UserOutlined />} title='User'>
-            <Menu.Item key='3'>Tom</Menu.Item>
-            <Menu.Item key='4'>Bill</Menu.Item>
-            <Menu.Item key='5'>Alex</Menu.Item>
-          </SubMenu>
-          <SubMenu key='sub2' icon={<TeamOutlined />} title='Team'>
-            <Menu.Item key='6'>Team 1</Menu.Item>
-            <Menu.Item key='8'>Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key='9' icon={<FileOutlined />}>
-            Files
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className='site-layout'>
-        <Header className='site-layout-background'>
+      <Header className='site-layout-background'>
+        <div
+          style={{
+            display: 'flex',
+            placeItems: 'center',
+            gap: '20px',
+          }}>
           <div
-            style={{
-              display: 'flex',
-              placeItems: 'center',
-              gap: '20px',
-              justifyContent: 'flex-end',
-            }}>
-            <div
-              style={{ fontWeight: 'bold', color: 'white', fontSize: '1.2em' }}>
-              {user}
-            </div>
-            <Button
-              type='primary'
-              onClick={async () => {
-                try {
-                  await logout()
-                } catch {}
-                window.location.replace('/login')
-              }}>
-              logout
-            </Button>
+            style={{ fontWeight: 'bold', color: 'white', fontSize: '2em' }}>
+            NAME
           </div>
-        </Header>
+          <div
+            style={{ fontWeight: 'bold', color: 'white', fontSize: '1.2em', marginLeft: 'auto' }}>
+            {user}
+          </div>
+          <Button
+            type='primary'
+            onClick={async () => {
+              try {
+                await logout()
+              } catch {}
+              window.location.replace('/login')
+            }}>
+            logout
+          </Button>
+        </div>
+      </Header>
+      <Layout className='site-layout'>
+        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+          <div className='logo' />
+          <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
+            <Menu.Item key='1' icon={<PieChartOutlined />}>
+              Option 1
+            </Menu.Item>
+            <Menu.Item key='2' icon={<DesktopOutlined />}>
+              Option 2
+            </Menu.Item>
+            <SubMenu key='sub1' icon={<UserOutlined />} title='User'>
+              <Menu.Item key='3'>Tom</Menu.Item>
+              <Menu.Item key='4'>Bill</Menu.Item>
+              <Menu.Item key='5'>Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key='sub2' icon={<TeamOutlined />} title='Team'>
+              <Menu.Item key='6'>Team 1</Menu.Item>
+              <Menu.Item key='8'>Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key='9' icon={<FileOutlined />}>
+              Files
+            </Menu.Item>
+          </Menu>
+        </Sider>
         <Content style={{ margin: '20px' }}>
           <div>
             <h1>
